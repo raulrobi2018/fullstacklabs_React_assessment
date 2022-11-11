a896d204be6378f0c63fa22be67af8219986beb6
Raul Rodriguez <raulrobi@gmail.com>
2022-11-11 10:02:32 -0300

    Challenge 1 and 2 solved

--- problem-solving/challenge-1/index.ts
+++ problem-solving/challenge-1/index.ts
@@ -1,1 +1,16 @@
-export const numbersFractionCalculator = (numbers: number[]) => {};
+export const numbersFractionCalculator = (numbers: number[]) => {
+
+    let positives: number = 0;
+    let negatives: number = 0;
+    let zeros: number = 0;
+
+    for (let i: number = 0; i < numbers.length; i++) {
+        numbers[i] > 0 ? positives++ : numbers[i] < 0 ? negatives++ : zeros++;
+    }
+
+    return {
+        positives: (positives / numbers.length).toFixed(6),
+        negative: (negatives / numbers.length).toFixed(6),
+        zeros: (zeros / numbers.length).toFixed(6),
+    }
+};
--- problem-solving/challenge-2/index.ts
+++ problem-solving/challenge-2/index.ts
@@ -3,5 +3,31 @@ export const diceFacesCalculator = (
   dice2: number,
   dice3: number
 ): number => {
+
+  let arr: number[] = [0, 0, 0, 0, 0, 0];
+  if (dice1 > 0 && dice1 < 7) {
+    arr[dice1 - 1]++;
+  }
+
+  if (dice2 > 0 && dice2 < 7) {
+    arr[dice2 - 1]++;
+  }
+  if (dice3 > 0 && dice3 < 7) {
+    arr[dice3 - 1]++;
+  }
+
+  if (dice1 <= 0 || dice1 > 6 || dice2 <= 0 || dice2 > 6 || dice3 <= 0 || dice3 > 6) {
+    throw new Error('Dice out of number range');
+  }
+
+  const max = Math.max(...arr);
+
+  if (max === 3) return (arr.indexOf(max) + 1) * 3;
+  if (max === 2) return (arr.indexOf(max) + 1) * 2;
+
+  for (let index: number = arr.length - 1; index >= 0; index--) {
+    if (arr[index] > 0) return index + 1;
+  }
+
   return 0;
 };
