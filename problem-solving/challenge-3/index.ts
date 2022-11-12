974e6f812e6dc6c207b2bbf2ea13ad102d28e7c9
Raul Rodriguez <raulrobi@gmail.com>
2022-11-12 10:32:13 -0300

    Testing

--- problem-solving/challenge-3/index.ts
+++ problem-solving/challenge-3/index.ts
@@ -1,3 +1,14 @@
 export const findLessCostPath = (board: number[][]): number => {
+
+  console.log("board", board);
+
+
+
   return 0;
 };
+
+const min = (a, b, c) => {
+  if (a < b) {
+    return (a < b) ? a : b;
+  }
+}
